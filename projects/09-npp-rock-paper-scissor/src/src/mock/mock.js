b08250d6d792114692c3b24c389452421b15a678
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-01-23 15:09:14 +0300

    Add Rock Paper Scissors App

---
+++ projects/09-npp-rock-paper-scissor/src/src/mock/mock.js
@@ -0,0 +1,18 @@
+import rock from '../assets/images/hand-rock.png';
+import paper from '../assets/images/hand.png';
+import scissors from '../assets/images/hand-scissors.png';
+
+export default [
+  {
+    name: 'rock',
+    src: rock,
+  },
+  {
+    name: 'paper',
+    src: paper,
+  },
+  {
+    name: 'scissors',
+    src: scissors,
+  },
+];
