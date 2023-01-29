1830f941f4342db1ba5013cc3849a56e07e5c569
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-01-30 00:30:07 +0300

    Update Guess Number App

--- projects/guess-number-01/source/src/components/GuessNumber.jsx
+++ projects/guess-number-01/source/src/components/GuessNumber.jsx
@@ -1,74 +1,36 @@
-import { FiGithub } from 'react-icons/fi';
-import { useState } from 'react';
-import { getRandomNumber } from './utils/getRandomNumber.js';
-import { toast, Toaster } from 'react-hot-toast';
+import { useRef, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { getRandomNumber } from '../utils/getRandomNumber.js';
 import confetti from 'canvas-confetti';
 
-/**
- * @component App - Main Component
- * @return {JSX.Element}
- * @constructor
- */
-const App = () => (
-  <>
-    <div className='npp'>
-      <div className='npp-app'>
-        <GuessNumber />
-        <Toaster position='bottom-center' />
-      </div>
-      <a className='npp-author' href='https://github.com/nagoev-alim' target='_blank'>
-        <FiGithub size={25} />
-      </a>
-    </div>
-  </>
-);
-
-export default App;
-
-
-/**
- * @function GuessNumber
- * @return {JSX.Element}
- * @constructor
- */
 const GuessNumber = () => {
-  // =====================
-  // 🚀 Hooks
-  // =====================
   const [setup, setSetup] = useState({
     secret: getRandomNumber(1, 10),
     attempts: 2,
   });
+  const inputRef = useRef();
 
   console.log(`👋 The number that was guessed is ${setup.secret}`);
 
-  // =====================
-  // 🚀 Methods
-  // =====================
-  /**
-   * @function onSubmit - Form submit handler
-   * @param event
-   */
+  // 🚀 METHODS: ===============================
   const onSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
     const guess = Number(Object.fromEntries(new FormData(form).entries()).guess);
 
-    // Validate
     if (guess < 0 || guess > 10 || guess === 0) {
       toast.error('Please enter a number from 1 to 10!');
       return;
     }
 
     if (guess === setup.secret) {
+      // Show Message
       toast((t) => (
-        <div className='custom'>
+        <div className='toast toast--success'>
           <p>You guessed it 🥳! The number you guessed - <span>{setup.secret}</span></p>
-          <button onClick={() => location.reload()}>Play again?</button>
+          <button className='button button--fluid button--green' onClick={() => location.reload()}>Play again?</button>
         </div>
-      ), {
-        duration: Infinity,
-      });
+      ), { duration: Infinity });
 
       confetti({
         angle: getRandomNumber(55, 125),
@@ -84,40 +46,37 @@ const GuessNumber = () => {
       setSetup(prevState => ({ ...prevState, attempts: prevState.attempts - 1 }));
 
       if (setup.attempts === 0) {
+        // Show Message
         toast((t) => (
-          <div className='custom error'>
+          <div className='toast toast--error'>
             <p>You lost 🥲! The number you guessed - <span>{setup.secret}</span></p>
-            <button onClick={() => location.reload()}>Play again?</button>
+            <button className='button button--fluid button--red' onClick={() => location.reload()}>Play again?
+            </button>
           </div>
         ), { duration: Infinity });
 
         form.remove();
       } else {
         toast.error(`Try again. Attempts left ${setup.attempts}`, { duration: 2000 });
-
-        form.querySelector('input').disabled = true;
-
+        inputRef.current.disabled = true;
         setTimeout(() => {
-          form.querySelector('input').disabled = false;
-          form.querySelector('input').focus();
+          inputRef.current.disabled = false;
+          inputRef.current.focus();
         }, 2000);
-
         form.reset();
       }
     }
   };
 
-  // =====================
-  // 🚀 Render
-  // =====================
-  return <>
-    <h1 className='title'>Guess Number</h1>
+  // 🚀 RENDER: ================================
+  return <div className='game'>
+    <h1 className='title game__title'>Guess Number</h1>
     <p>Guess the number is a game in which you have to guess the number given by the computer between 1 and 10. Use as
       few tries as possible. Good luck!</p>
     <form onSubmit={onSubmit}>
-      <label>
-        <input type='number' name='guess' min='1' max='10' placeholder='Enter the number' />
-      </label>
+      <input ref={inputRef} type='number' name='guess' min='1' max='10' placeholder='Enter the number' />
     </form>
-  </>;
+  </div>;
 };
+
+export default GuessNumber;
