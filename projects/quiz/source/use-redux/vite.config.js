bb2ffc70b394b8034925b06784428a04929eb419
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-01-30 14:12:45 +0300

    Update Simple Converters Project

--- projects/quiz/source/use-redux/vite.config.js
+++ projects/quiz/source/use-redux/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  // base: '',
   // root: 'src',
-  build: {
-    outDir: '../dist',
-  },
+  // build: {
+  //   outDir: '../dist',
+  // },
 })
---
+++ projects/simple-converters/source/src/App.jsx
@@ -0,0 +1,9 @@
+import { AppWrapper, Converter } from './components/index.js';
+
+const App = () => (
+  <AppWrapper>
+    <Converter />
+  </AppWrapper>
+);
+
+export default App;
