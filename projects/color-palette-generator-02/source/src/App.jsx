f3c19e5fc5adf90710a106bb0012eb8f5c46e783
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-02-01 19:48:31 +0300

    Update Color Palette Generator App

---
+++ projects/color-palette-generator-02/source/src/App.jsx
@@ -0,0 +1,9 @@
+import { AppWrapper, ColorGenerator } from './components/index.js';
+
+const App = () => (
+  <AppWrapper>
+    <ColorGenerator />
+  </AppWrapper>
+);
+
+export default App;
---
+++ projects/color-palette-generator-02/source/src/components/layout/AppWrapper.jsx
@@ -0,0 +1,18 @@
+import { FiGithub } from 'react-icons/fi';
+import { Toaster } from 'react-hot-toast';
+
+const AppWrapper = ({ children }) => (
+  <div className='npp'>
+    <div className='npp-app'>
+
+      {children}
+
+      <a className='npp-author' href='https://github.com/nagoev-alim' target='_blank'>
+        <FiGithub size={25} />
+      </a>
+    </div>
+    <Toaster position='bottom-center' />
+  </div>
+);
+
+export default AppWrapper;
