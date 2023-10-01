a51959d7985932b3b17a140f1159d89517a9ad28
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-01 14:12:37 +0300

    Remove stopwatch

--- projects/stopwatch/source/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { AppWrapper, StopWatch } from './components/index.js';
-
-const App = () => (
-  <AppWrapper>
-    <StopWatch />
-  </AppWrapper>
-);
-
-export default App;
--- projects/stopwatch/source/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// App Wrapper
-export { default as AppWrapper } from './layout/AppWrapper.jsx';
-// App
-export { default as StopWatch } from './StopWatch.jsx';
-// Layout
-// export { default as Example } from './layout/Example.jsx';
-// Pages
-// export { default as Example } from './pages/Example.jsx';
