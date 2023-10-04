600b034dcb256f5d94694ffd2d7f5a0b820f0af2
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-04 13:54:57 +0300

    Remove custom-pagination-02

--- projects/custom-pagination-02/source/src/components/index.js
+++
@@ -1,9 +0,0 @@
-// App Wrapper
-export { default as AppWrapper } from './layout/AppWrapper.jsx';
-// App
-export { default as CustomPagination } from './CustomPagination.jsx';
-// Layout
-export { default as Users } from './layout/Users.jsx';
-export { default as Pagination } from './layout/Pagination.jsx';
-// Pages
-// export { default as SetupPage } from './pages/SetupPage.jsx';
