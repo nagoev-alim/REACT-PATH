6bbaf3d655ac21e1b28c4a06f43ae7ed9ac20ce8
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-04 21:52:00 +0300

    Remove password-generator

--- projects/password-generator/source/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { AppWrapper, PasswordGenerator } from './components/index.js';
-
-const App = () => (
-  <AppWrapper>
-    <PasswordGenerator />
-  </AppWrapper>
-);
-
-export default App;
--- projects/password-generator/source/src/components/index.js
+++
@@ -1,8 +0,0 @@
-// App Wrapper
-export { default as AppWrapper } from './layout/AppWrapper.jsx';
-// App
-export { default as PasswordGenerator } from './PasswordGenerator.jsx';
-// Layout
-// export { default as Form } from './layout/Form.jsx';
-// Pages
-// export { default as SetupPage } from './pages/SetupPage.jsx';
