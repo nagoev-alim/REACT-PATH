a1f5221473ddeb97d6da58943c207894133d4737
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-05 12:44:37 +0300

    Remove products-filter-02

--- projects/products-filter-02/source/src/utils/getRandomNumber.js
+++
@@ -1,7 +0,0 @@
-/**
- * @function getRandomNumber - Get random number
- * @param min
- * @param max
- * @returns {number}
- */
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
