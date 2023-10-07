0658e62e478a8435977ed816290198d75f67ce51
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-07 14:15:50 +0300

    Remove search-apis

--- projects/search-apis/source/01-use-context/src/components/index.js
+++
@@ -1,9 +0,0 @@
-// App Wrapper
-export { default as AppWrapper } from './UI/AppWrapper/AppWrapper.jsx';
-export { default as Loader } from './UI/Loader/Loader.jsx';
-export { default as Error } from './UI/Error/Error.jsx';
-
-//
-export { default as Form } from './UI/Form/Form.jsx';
-export { default as Categories } from './UI/Categories/Categories.jsx';
-export { default as List } from './UI/List/List.jsx';
