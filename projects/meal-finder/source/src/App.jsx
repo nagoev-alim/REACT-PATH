492204ee997cbad8e64819b27e8b8a858619f9af
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-07 14:14:51 +0300

    Remove meal-finder

--- projects/meal-finder/source/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { AppWrapper, MealFinder } from './components/index.js';
-
-const App = () => (
-  <AppWrapper>
-    <MealFinder />
-  </AppWrapper>
-);
-
-export default App;
--- projects/meal-finder/source/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AppWrapper } from './layout/AppWrapper.jsx';
-export { default as MainPage } from './pages/MainPage.jsx';
-export { default as DetailPage } from './pages/DetailPage.jsx';
-export { default as MealFinder } from './MealFinder.jsx';
