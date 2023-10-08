1c03863c9c9b5eb0c1bf4b48d8ccfe0a5c4d6f05
Alim Nagoev <nagoev.personal@gmail.com>
2023-10-08 20:15:55 +0300

    Remove shopping-cart-01

--- projects/shopping-cart-01/source/src/components/layout/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import { LineWobble } from '@uiball/loaders';
-
-const Loading = () => (
-  <div className='loader'>
-    <LineWobble size={120} lineWeight={5} speed={2.75} />
-  </div>
-);
-
-export default Loading;
